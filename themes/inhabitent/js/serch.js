f354ba607209d452a0257466f5100bfdc7824b8a
Layth-ele <layth.ele@gmail.com>
2016-12-04 20:47:41 -0800

    fexing jQuery toggle function for search bar

--- themes/inhabitent/js/serch.js
+++ themes/inhabitent/js/serch.js
@@ -1,16 +1,13 @@
-/// this function showes and hide a search fiels=d
+/ this function showes and hide a search fiels=d
 
 jQuery(document).ready(function( $ ) {
 
- $(".main-navigation .search-field").hide();
-    $(".main-navigation .search-submit").click(function(){
-        $(".main-navigation .search-field").toggle("slide").focus();
+ $(".search-field").hide();
+    $(".search-submit").click(function(){
+        $(".search-field").toggle("slide").focus();
     })
 
 });
-
-
-
 
 
 
